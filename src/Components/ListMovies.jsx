acdddc8ef54738679be8f66fbb76e5d0e6e247d5
Layla Rodas <rodas.layla@gmail.com>
2024-01-30 11:59:37 +0100

    creacion del componente edit

--- src/Components/ListMovies.jsx
+++ src/Components/ListMovies.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react'
+import { Edit } from './Edit';
 
 export const ListMovies = ({listState, setListState}) => {
 
   //const [listState, setListState] = useState([]);
 
+  const [edit, setEdit] = useState(0);
+  
   useEffect(() => {
     console.log("Cargando componente listado de peliculas")
     getMovies();
@@ -43,8 +46,18 @@ export const ListMovies = ({listState, setListState}) => {
               <article key={movie.id} className='movieItem'>
                 <h3 className='title'>{movie.title}</h3>
                 <p className='description'>{movie.description}</p>
-                <button className='edit'>Edit</button>
+                <button className='edit' onClick={
+                  () => setEdit(movie.id)
+                }>Edit</button>
                 <button className='delete' onClick={() => { deleteMovie(movie.id)}}>Delete</button>
+
+                {/* Edit Form*/}
+                {
+                  edit === movie.id && (
+                    <Edit />
+                  )
+                }
+
               </article>
             )
         })
