f00aa124753ef16c2bf00401fdb74ddc7ca3672f
Layla Rodas <rodas.layla@gmail.com>
2024-01-30 23:36:39 +0100

    mensaje de no encontrado

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -4,6 +4,7 @@ import React, { useState } from 'react'
 export const Search = ({listState, setListState}) => {
 
   const [search, setSearch] = useState('');
+  const [notFound, setNotFound] = useState(false);
 
   const searchMovie = (e) => {
     //crear estado y actualizarlo
@@ -17,17 +18,23 @@ export const Search = ({listState, setListState}) => {
 
     if(search.length <= 1 || foundMovies <= 0){
       foundMovies = JSON.parse(localStorage.getItem("movies"));
+      setNotFound(true);
+    }else{
+      setNotFound(false);
     }
+
+
     //console.log(foundMovies);
     setListState(foundMovies);
-
-    //actualizar estado
 
   }
 
   return (
     <div className='search'>
        <h3 className='title'>Search</h3>
+       { (notFound == true && search.length > 1 && 
+            (<span className='notFound'>No match found</span>)
+       )}
         <form action="">
             <input type="text" id="searchField" name='search' autoComplete='off' onChange={ searchMovie }/>
             <button>Submit</button>
